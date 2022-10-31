0b05bc9e6a99d606c56380ac483d13a4ad9b74a4
panpf <panpfpanpf@outlook.com>
2022-10-31 11:29:03 +0800

    new: Added Column, FlowRow, FlowColumn samples

--- compose/src/main/java/com/github/panpf/android/compose/samples/ui/layouts/FlowColumnFragment.kt
+++ compose/src/main/java/com/github/panpf/android/compose/samples/ui/layouts/FlowColumnFragment.kt
@@ -5,12 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Chip
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,6 +29,11 @@ import com.github.panpf.android.compose.samples.ui.base.ExpandableItem
 import com.github.panpf.android.compose.samples.ui.base.ExpandableLayout
 import com.github.panpf.android.compose.samples.ui.base.ToolbarFragment
 import com.github.panpf.android.compose.samples.ui.base.theme.MyTheme
+import com.google.accompanist.flowlayout.FlowColumn
+import com.google.accompanist.flowlayout.FlowCrossAxisAlignment
+import com.google.accompanist.flowlayout.FlowMainAxisAlignment
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.SizeMode
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -29,7 +45,7 @@ class FlowColumnFragment : ToolbarFragment() {
         parent: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        toolbar.title = "Column"
+        toolbar.title = "FlowColumn"
         return ComposeView(inflater.context).apply {
             setContent {
                 MyTheme {
@@ -39,6 +55,13 @@ class FlowColumnFragment : ToolbarFragment() {
                     ) {
                         ExpandableLayout { allExpandFlow ->
                             FlowColumnSample(allExpandFlow)
+                            FlowColumnFullSample(allExpandFlow)
+                            FlowColumnMainAxisSizeSample(allExpandFlow)
+                            FlowColumnMainAxisAlignmentSample(allExpandFlow)
+                            FlowColumnMainAxisSpacingSample(allExpandFlow)
+                            FlowColumnCrossAxisAlignmentSample(allExpandFlow)
+                            FlowColumnCrossAxisSpacingSample(allExpandFlow)
+                            FlowColumnLastLineMainAxisAlignmentSample(allExpandFlow)
                         }
                     }
                 }
@@ -48,10 +71,21 @@ class FlowColumnFragment : ToolbarFragment() {
 }
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun FlowColumnSample(allExpandFlow: Flow<Boolean>) {
     ExpandableItem(title = "FlowColumn", allExpandFlow, padding = 20.dp) {
-        // todo waiting implementation
+        FlowColumn(
+            modifier = Modifier
+                .height(200.dp)
+                .background(Color.Red.copy(alpha = 0.5f))
+        ) {
+            listOf("数\n码", "汽\n车", "摄\n影").forEach {
+                Chip(onClick = { }) {
+                    Text(text = it)
+                }
+            }
+        }
     }
 }
 
@@ -59,4 +93,252 @@ fun FlowColumnSample(allExpandFlow: Flow<Boolean>) {
 @Composable
 fun FlowColumnSamplePreview() {
     FlowColumnSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowColumnFullSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowColumn（Full）", allExpandFlow, padding = 20.dp) {
+        FlowColumn(
+            modifier = Modifier
+                .height(200.dp)
+                .background(Color.Red.copy(alpha = 0.5f))
+        ) {
+            listOf("数\n码", "汽\n车", "摄\n影", "舞\n蹈", "音\n乐", "科\n技").forEach {
+                Chip(onClick = { }) {
+                    Text(text = it)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowColumnFullSamplePreview() {
+    FlowColumnFullSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowColumnMainAxisSizeSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowColumn（mainAxisSize）", allExpandFlow, padding = 20.dp) {
+        Row(Modifier.height(240.dp)) {
+            listOf(
+                SizeMode.Wrap to "Wrap",
+                SizeMode.Expand to "Expand",
+            ).forEachIndexed { index, (sizeMode, name) ->
+                if (index > 0) {
+                    Spacer(modifier = Modifier.size(10.dp))
+                }
+                Column {
+                    Text(text = name, modifier = Modifier.height(46.dp))
+                    FlowColumn(
+                        modifier = Modifier.background(Color.Red.copy(alpha = 0.5f)),
+                        mainAxisSize = sizeMode
+                    ) {
+                        listOf("数\n码", "汽\n车", "摄\n影").forEach {
+                            Chip(onClick = { }) {
+                                Text(text = it)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowColumnMainAxisSizeSamplePreview() {
+    FlowColumnMainAxisSizeSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowColumnMainAxisAlignmentSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowColumn（mainAxisAlignment）", allExpandFlow, padding = 20.dp) {
+        FlowRow(mainAxisSpacing = 10.dp, crossAxisSpacing = 10.dp) {
+            listOf(
+                FlowMainAxisAlignment.Start to "Start",
+                FlowMainAxisAlignment.Center to "Center",
+                FlowMainAxisAlignment.End to "End",
+                FlowMainAxisAlignment.SpaceBetween to "Space\nBetween",
+                FlowMainAxisAlignment.SpaceAround to "Space\nAround",
+                FlowMainAxisAlignment.SpaceEvenly to "Space\nEvenly",
+            ).forEach { (alignment, name) ->
+                Column {
+                    Text(text = name, modifier = Modifier.height(46.dp))
+                    FlowColumn(
+                        modifier = Modifier
+                            .height(200.dp)
+                            .background(Color.Red.copy(alpha = 0.5f)),
+                        mainAxisAlignment = alignment
+                    ) {
+                        listOf(
+                            "数\n码",
+                            "汽\n车",
+                            "摄\n影",
+                        ).forEach {
+                            Chip(onClick = { }) {
+                                Text(text = it)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowColumnMainAxisAlignmentSamplePreview() {
+    FlowColumnMainAxisAlignmentSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowColumnMainAxisSpacingSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowColumn（mainAxisSpacing）", allExpandFlow, padding = 20.dp) {
+        FlowColumn(
+            modifier = Modifier
+                .height(200.dp)
+                .background(Color.Red.copy(alpha = 0.5f)),
+            mainAxisSpacing = 10.dp
+        ) {
+            listOf("数\n码", "汽\n车", "摄\n影", "舞\n蹈", "音\n乐").forEach {
+                Chip(onClick = { }) {
+                    Text(text = it)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowColumnMainAxisSpacingSamplePreview() {
+    FlowColumnMainAxisSpacingSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowColumnCrossAxisAlignmentSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowColumn（crossAxisAlignment）（无效）", allExpandFlow, padding = 20.dp) {
+        FlowRow(mainAxisSpacing = 10.dp, crossAxisSpacing = 10.dp) {
+            listOf(
+                FlowCrossAxisAlignment.Start to "Start",
+                FlowCrossAxisAlignment.Center to "Center",
+                FlowCrossAxisAlignment.End to "End",
+            ).forEach { (alignment, name) ->
+                Column {
+                    Text(text = name, modifier = Modifier.height(46.dp))
+                    FlowColumn(
+                        modifier = Modifier
+                            .height(200.dp)
+                            .width(100.dp)
+                            .background(Color.Red.copy(alpha = 0.5f)),
+                        crossAxisAlignment = alignment  // todo 无效
+                    ) {
+                        listOf("数\n码", "汽\n车", "摄\n影").forEach {
+                            Chip(onClick = { }) {
+                                Text(text = it)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowColumnCrossAxisAlignmentSamplePreview() {
+    FlowColumnCrossAxisAlignmentSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowColumnCrossAxisSpacingSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowColumn（crossAxisSpacing）", allExpandFlow, padding = 20.dp) {
+        FlowColumn(
+            modifier = Modifier
+                .height(200.dp)
+                .background(Color.Red.copy(alpha = 0.5f)),
+            crossAxisSpacing = 16.dp
+        ) {
+            listOf("数\n码", "汽\n车", "摄\n影", "舞\n蹈", "音\n乐").forEach {
+                Chip(onClick = { }) {
+                    Text(text = it)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowColumnCrossAxisSpacingSamplePreview() {
+    FlowColumnCrossAxisSpacingSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowColumnLastLineMainAxisAlignmentSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(
+        title = "FlowColumn（lastLineMainAxisAlignment）",
+        allExpandFlow,
+        padding = 20.dp
+    ) {
+        FlowRow(mainAxisSpacing = 10.dp, crossAxisSpacing = 10.dp) {
+            listOf(
+                FlowMainAxisAlignment.Start to "Start",
+                FlowMainAxisAlignment.Center to "Center",
+                FlowMainAxisAlignment.End to "End",
+                FlowMainAxisAlignment.SpaceBetween to "Space\nBetween",
+                FlowMainAxisAlignment.SpaceAround to "Space\nAround",
+                FlowMainAxisAlignment.SpaceEvenly to "Space\nEvenly",
+            ).forEach { (alignment, name) ->
+                Column {
+                    Text(text = name, modifier = Modifier.height(46.dp))
+                    FlowColumn(
+                        modifier = Modifier
+                            .height(200.dp)
+                            .background(Color.Red.copy(alpha = 0.5f)),
+                        lastLineMainAxisAlignment = alignment
+                    ) {
+                        listOf(
+                            "数\n码",
+                            "汽\n车",
+                            "摄\n影",
+                            "舞\n蹈",
+                            "音\n乐",
+                            "科\n技",
+                            "健\n身",
+                        ).forEach {
+                            Chip(onClick = { }) {
+                                Text(text = it)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowColumnLastLineMainAxisAlignmentSamplePreview() {
+    FlowColumnLastLineMainAxisAlignmentSample(remember { MutableStateFlow(true) })
 }
--- compose/src/main/java/com/github/panpf/android/compose/samples/ui/layouts/FlowRowFragment.kt
+++ compose/src/main/java/com/github/panpf/android/compose/samples/ui/layouts/FlowRowFragment.kt
@@ -5,12 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Chip
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,7 +28,10 @@ import com.github.panpf.android.compose.samples.ui.base.ExpandableItem
 import com.github.panpf.android.compose.samples.ui.base.ExpandableLayout
 import com.github.panpf.android.compose.samples.ui.base.ToolbarFragment
 import com.github.panpf.android.compose.samples.ui.base.theme.MyTheme
+import com.google.accompanist.flowlayout.FlowCrossAxisAlignment
+import com.google.accompanist.flowlayout.FlowMainAxisAlignment
 import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.SizeMode
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -30,7 +43,7 @@ class FlowRowFragment : ToolbarFragment() {
         parent: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        toolbar.title = "Row"
+        toolbar.title = "FlowRow"
         return ComposeView(inflater.context).apply {
             setContent {
                 MyTheme {
@@ -40,6 +53,13 @@ class FlowRowFragment : ToolbarFragment() {
                     ) {
                         ExpandableLayout { allExpandFlow ->
                             FlowRowSample(allExpandFlow)
+                            FlowRowFullSample(allExpandFlow)
+                            FlowRowMainAxisSizeSample(allExpandFlow)
+                            FlowRowMainAxisAlignmentSample(allExpandFlow)
+                            FlowRowMainAxisSpacingSample(allExpandFlow)
+                            FlowRowCrossAxisAlignmentSample(allExpandFlow)
+                            FlowRowCrossAxisSpacingSample(allExpandFlow)
+                            FlowRowLastLineMainAxisAlignmentSample(allExpandFlow)
                         }
                     }
                 }
@@ -49,13 +69,21 @@ class FlowRowFragment : ToolbarFragment() {
 }
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun FlowRowSample(allExpandFlow: Flow<Boolean>) {
     ExpandableItem(title = "FlowRow", allExpandFlow, padding = 20.dp) {
-        // todo waiting implementation
-//        FlowRow() {
-//
-//        }
+        FlowRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red.copy(alpha = 0.5f))
+        ) {
+            listOf("数码", "汽车", "摄影", "舞蹈").forEach {
+                Chip(onClick = { }) {
+                    Text(text = it)
+                }
+            }
+        }
     }
 }
 
@@ -63,4 +91,237 @@ fun FlowRowSample(allExpandFlow: Flow<Boolean>) {
 @Composable
 fun FlowRowSamplePreview() {
     FlowRowSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowRowFullSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowRow（Full）", allExpandFlow, padding = 20.dp) {
+        FlowRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red.copy(alpha = 0.5f))
+        ) {
+            listOf("数码", "汽车", "摄影", "舞蹈", "二次元", "音乐", "科技", "健身", "游戏", "文学").forEach {
+                Chip(onClick = { }) {
+                    Text(text = it)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowRowFullSamplePreview() {
+    FlowRowFullSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowRowMainAxisSizeSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowRow（mainAxisSize）", allExpandFlow, padding = 20.dp) {
+        Column {
+            listOf(
+                SizeMode.Wrap to "Wrap",
+                SizeMode.Expand to "Expand",
+            ).forEachIndexed { index, (sizeMode, name) ->
+                if (index > 0) {
+                    Spacer(modifier = Modifier.size(10.dp))
+                }
+                Text(text = name)
+                FlowRow(
+                    modifier = Modifier.background(Color.Red.copy(alpha = 0.5f)),
+                    mainAxisSize = sizeMode
+                ) {
+                    listOf("数码", "汽车", "摄影", "舞蹈").forEach {
+                        Chip(onClick = { }) {
+                            Text(text = it)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowRowMainAxisSizeSamplePreview() {
+    FlowRowMainAxisSizeSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowRowMainAxisAlignmentSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowRow（mainAxisAlignment）", allExpandFlow, padding = 20.dp) {
+        Column {
+            listOf(
+                FlowMainAxisAlignment.Start to "Start",
+                FlowMainAxisAlignment.Center to "Center",
+                FlowMainAxisAlignment.End to "End",
+                FlowMainAxisAlignment.SpaceBetween to "SpaceBetween",
+                FlowMainAxisAlignment.SpaceAround to "SpaceAround",
+                FlowMainAxisAlignment.SpaceEvenly to "SpaceEvenly",
+            ).forEachIndexed { index, (alignment, name) ->
+                if (index > 0) {
+                    Spacer(modifier = Modifier.size(10.dp))
+                }
+                Text(text = name)
+                FlowRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Red.copy(alpha = 0.5f)),
+                    mainAxisAlignment = alignment
+                ) {
+                    listOf("数码", "汽车", "摄影", "舞蹈", "二次元", "音乐", "科技", "健身", "游戏", "文学").forEach {
+                        Chip(onClick = { }) {
+                            Text(text = it)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowRowMainAxisAlignmentSamplePreview() {
+    FlowRowMainAxisAlignmentSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowRowMainAxisSpacingSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowRow（mainAxisSpacing）", allExpandFlow, padding = 20.dp) {
+        FlowRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red.copy(alpha = 0.5f)),
+            mainAxisSpacing = 10.dp
+        ) {
+            listOf("数码", "汽车", "摄影", "舞蹈", "二次元", "音乐", "科技", "健身", "游戏", "文学").forEach {
+                Chip(onClick = { }) {
+                    Text(text = it)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowRowMainAxisSpacingSamplePreview() {
+    FlowRowMainAxisSpacingSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowRowCrossAxisAlignmentSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowRow（crossAxisAlignment）（无效）", allExpandFlow, padding = 20.dp) {
+        Column {
+            listOf(
+                FlowCrossAxisAlignment.Start to "Start",
+                FlowCrossAxisAlignment.Center to "Center",
+                FlowCrossAxisAlignment.End to "End",
+            ).forEachIndexed { index, (alignment, name) ->
+                if (index > 0) {
+                    Spacer(modifier = Modifier.size(10.dp))
+                }
+                Text(text = name)
+                FlowRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                        .background(Color.Red.copy(alpha = 0.5f)),
+                    crossAxisAlignment = alignment  // todo 无效
+                ) {
+                    listOf("数码", "汽车", "摄影", "舞蹈").forEach {
+                        Chip(onClick = { }) {
+                            Text(text = it)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowRowCrossAxisAlignmentSamplePreview() {
+    FlowRowCrossAxisAlignmentSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowRowCrossAxisSpacingSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowRow（crossAxisSpacing）", allExpandFlow, padding = 20.dp) {
+        FlowRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red.copy(alpha = 0.5f)),
+            crossAxisSpacing = 16.dp
+        ) {
+            listOf("数码", "汽车", "摄影", "舞蹈", "二次元", "音乐", "科技", "健身", "游戏", "文学").forEach {
+                Chip(onClick = { }) {
+                    Text(text = it)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowRowCrossAxisSpacingSamplePreview() {
+    FlowRowCrossAxisSpacingSample(remember { MutableStateFlow(true) })
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FlowRowLastLineMainAxisAlignmentSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem(title = "FlowRow（lastLineMainAxisAlignment）", allExpandFlow, padding = 20.dp) {
+        Column {
+            listOf(
+                FlowMainAxisAlignment.Start to "Start",
+                FlowMainAxisAlignment.Center to "Center",
+                FlowMainAxisAlignment.End to "End",
+                FlowMainAxisAlignment.SpaceBetween to "SpaceBetween",
+                FlowMainAxisAlignment.SpaceAround to "SpaceAround",
+                FlowMainAxisAlignment.SpaceEvenly to "SpaceEvenly",
+            ).forEachIndexed { index, (alignment, name) ->
+                if (index > 0) {
+                    Spacer(modifier = Modifier.size(10.dp))
+                }
+                Text(text = name)
+                FlowRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Red.copy(alpha = 0.5f)),
+                    lastLineMainAxisAlignment = alignment
+                ) {
+                    listOf("数码", "汽车", "摄影", "舞蹈", "二次元", "音乐", "科技", "健身", "游戏", "文学").forEach {
+                        Chip(onClick = { }) {
+                            Text(text = it)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun FlowRowLastLineMainAxisAlignmentSamplePreview() {
+    FlowRowLastLineMainAxisAlignmentSample(remember { MutableStateFlow(true) })
 }
