ff53bd6dbf8f1a4ff609d44af2d9c2f4aab48496
panpf <panpfpanpf@outlook.com>
2022-12-21 15:21:45 +0800

    new: Added Modifier absoluteOffset, absolutePadding samples

--- compose/src/main/java/com/github/panpf/android/compose/samples/ui/modifier/ModifierOffsetFragment.kt
+++ compose/src/main/java/com/github/panpf/android/compose/samples/ui/modifier/ModifierOffsetFragment.kt
@@ -3,13 +3,17 @@ package com.github.panpf.android.compose.samples.ui.modifier
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.github.panpf.android.compose.samples.ui.base.DescItem
 import com.github.panpf.android.compose.samples.ui.base.ExpandableItem3
@@ -29,7 +33,7 @@ class ModifierOffsetFragment : Material3ComposeAppBarFragment() {
     override fun DrawContent() {
         ExpandableLayout { allExpandFlow ->
             ModifierOffsetSample(allExpandFlow)
-            // todo absoluteOffset 因为还没有找到修改 LayoutDirection 的方法
+            ModifierAbsoluteOffsetSample(allExpandFlow)
         }
     }
 }
@@ -39,48 +43,104 @@ class ModifierOffsetFragment : Material3ComposeAppBarFragment() {
 private fun ModifierOffsetSample(allExpandFlow: Flow<Boolean>) {
     ExpandableItem3(title = "Modifier（offset）", allExpandFlow, padding = 20.dp) {
         val colorScheme = MaterialTheme.colorScheme
-        FlowRow(mainAxisSpacing = 10.dp, crossAxisSpacing = 10.dp) {
-            DescItem("no offset") {
+        FlowRow(mainAxisSpacing = 30.dp, crossAxisSpacing = 30.dp) {
+            DescItem("LTR\nno offset") {
                 Box(
                     modifier = Modifier
                         .size(100.dp)
                         .border(2.dp, colorScheme.primary)
                 ) {
-                    Box(
-                        modifier = Modifier
-                            .size(60.dp)
-                            .background(colorScheme.primaryContainer)
-                    )
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
                 }
             }
 
-            DescItem("x=20.dp, y=30.dp") {
+            DescItem("RTL\nno offset") {
                 Box(
                     modifier = Modifier
                         .size(100.dp)
                         .border(2.dp, colorScheme.primary)
                 ) {
-                    Box(
-                        modifier = Modifier
-                            .size(60.dp)
-                            .offset(x = 20.dp, y = 30.dp)
-                            .background(colorScheme.primaryContainer)
-                    )
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
                 }
             }
 
-            DescItem("x=-4.dp, y=-4.dp") {
+            DescItem("LTR\nx=10.dp, y=10.dp") {
                 Box(
                     modifier = Modifier
                         .size(100.dp)
                         .border(2.dp, colorScheme.primary)
                 ) {
-                    Box(
-                        modifier = Modifier
-                            .size(60.dp)
-                            .offset(x = (-4).dp, y = (-4).dp)
-                            .background(colorScheme.primaryContainer)
-                    )
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .offset(x = 10.dp, y = 10.dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
+                }
+            }
+
+            DescItem("RTL\nx=10.dp, y=10.dp") {
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .border(2.dp, colorScheme.primary)
+                ) {
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .offset(x = 10.dp, y = 10.dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
+                }
+            }
+
+            DescItem("LTR\nx=-4.dp, y=-4.dp") {
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .border(2.dp, colorScheme.primary)
+                ) {
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .offset(x = (-4).dp, y = (-4).dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
+                }
+            }
+
+            DescItem("RTL\nx=-4.dp, y=-4.dp") {
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .border(2.dp, colorScheme.primary)
+                ) {
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .offset(x = (-4).dp, y = (-4).dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
                 }
             }
         }
@@ -91,4 +151,119 @@ private fun ModifierOffsetSample(allExpandFlow: Flow<Boolean>) {
 @Composable
 private fun ModifierOffsetSamplePreview() {
     ModifierOffsetSample(remember { MutableStateFlow(true) })
+}
+
+
+@Composable
+private fun ModifierAbsoluteOffsetSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem3(title = "Modifier（absoluteOffset）", allExpandFlow, padding = 20.dp) {
+        val colorScheme = MaterialTheme.colorScheme
+        FlowRow(mainAxisSpacing = 30.dp, crossAxisSpacing = 30.dp) {
+            DescItem("LTR\nno absoluteOffset") {
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .border(2.dp, colorScheme.primary)
+                ) {
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
+                }
+            }
+
+            DescItem("RTL\nno absoluteOffset") {
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .border(2.dp, colorScheme.primary)
+                ) {
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
+                }
+            }
+
+            DescItem("LTR\nx=10.dp, y=10.dp") {
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .border(2.dp, colorScheme.primary)
+                ) {
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .absoluteOffset(x = 10.dp, y = 10.dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
+                }
+            }
+
+            DescItem("RTL\nx=10.dp, y=10.dp") {
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .border(2.dp, colorScheme.primary)
+                ) {
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .absoluteOffset(x = 10.dp, y = 10.dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
+                }
+            }
+
+            DescItem("LTR\nx=-4.dp, y=-4.dp") {
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .border(2.dp, colorScheme.primary)
+                ) {
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .absoluteOffset(x = (-4).dp, y = (-4).dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
+                }
+            }
+
+            DescItem("RTL\nx=-4.dp, y=-4.dp") {
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .border(2.dp, colorScheme.primary)
+                ) {
+                    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .absoluteOffset(x = (-4).dp, y = (-4).dp)
+                                .background(colorScheme.primaryContainer)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+private fun ModifierAbsoluteOffsetSamplePreview() {
+    ModifierAbsoluteOffsetSample(remember { MutableStateFlow(true) })
 }
--- compose/src/main/java/com/github/panpf/android/compose/samples/ui/modifier/ModifierPaddingFragment.kt
+++ compose/src/main/java/com/github/panpf/android/compose/samples/ui/modifier/ModifierPaddingFragment.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
@@ -11,10 +12,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.github.panpf.android.compose.samples.ui.base.DescItem
 import com.github.panpf.android.compose.samples.ui.base.ExpandableItem3
@@ -36,9 +40,9 @@ class ModifierPaddingFragment : Material3ComposeAppBarFragment() {
     override fun DrawContent() {
         ExpandableLayout { allExpandFlow ->
             ModifierPaddingSample(allExpandFlow)
+            ModifierAbsolutePaddingSample(allExpandFlow)
             ModifierPaddingFromSample(allExpandFlow)
             ModifierPaddingWithBackgroundSample(allExpandFlow)
-            // todo absolutePadding 因为还没有找到修改 LayoutDirection 的方法
             // todo captionBarPadding
             // todo displayCutoutPadding
             // todo imePadding
@@ -75,7 +79,7 @@ private fun ModifierPaddingSample(allExpandFlow: Flow<Boolean>) {
         }
 
         Spacer(modifier = Modifier.size(10.dp))
-        DescItem("padding(all = 4.dp)") {
+        DescItem("padding(all=4.dp)") {
             Box(
                 modifier = Modifier
                     .size(60.dp)
@@ -91,7 +95,7 @@ private fun ModifierPaddingSample(allExpandFlow: Flow<Boolean>) {
         }
 
         Spacer(modifier = Modifier.size(10.dp))
-        DescItem("padding(horizontal = 4.dp, vertical = 8.dp)") {
+        DescItem("padding(horizontal=4.dp, vertical=8.dp)") {
             Box(
                 modifier = Modifier
                     .size(60.dp)
@@ -107,18 +111,38 @@ private fun ModifierPaddingSample(allExpandFlow: Flow<Boolean>) {
         }
 
         Spacer(modifier = Modifier.size(10.dp))
-        DescItem("padding(start = 2.dp, top = 4.dp, end = 6.dp, bottom = 8.dp)") {
-            Box(
-                modifier = Modifier
-                    .size(60.dp)
-                    .background(colorScheme.primaryContainer)
-                    .padding(start = 2.dp, top = 4.dp, end = 6.dp, bottom = 8.dp)
-            ) {
+        DescItem("LTR\npadding(start=2.dp, top=4.dp, end=6.dp, bottom=8.dp)") {
+            CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
                 Box(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .background(colorScheme.primary)
-                )
+                        .size(60.dp)
+                        .background(colorScheme.primaryContainer)
+                        .padding(start = 2.dp, top = 4.dp, end = 6.dp, bottom = 8.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(colorScheme.primary)
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.size(10.dp))
+        DescItem("RTL\npadding(start=2.dp, top=4.dp, end=6.dp, bottom=8.dp)") {
+            CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                Box(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .background(colorScheme.primaryContainer)
+                        .padding(start = 2.dp, top = 4.dp, end = 6.dp, bottom = 8.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(colorScheme.primary)
+                    )
+                }
             }
         }
     }
@@ -128,6 +152,69 @@ private fun ModifierPaddingSample(allExpandFlow: Flow<Boolean>) {
 @Composable
 private fun ModifierPaddingSamplePreview() {
     ModifierPaddingSample(remember { MutableStateFlow(true) })
+}
+
+
+@Composable
+private fun ModifierAbsolutePaddingSample(allExpandFlow: Flow<Boolean>) {
+    ExpandableItem3(title = "Modifier（absolutePadding）", allExpandFlow, padding = 20.dp) {
+        val colorScheme = MaterialTheme.colorScheme
+        DescItem("no padding") {
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(colorScheme.primaryContainer)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(colorScheme.primary)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.size(10.dp))
+        DescItem("LTR\npadding(left=2.dp, top=4.dp, right=6.dp, bottom=8.dp)") {
+            CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                Box(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .background(colorScheme.primaryContainer)
+                        .absolutePadding(left = 2.dp, top = 4.dp, right = 6.dp, bottom = 8.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(colorScheme.primary)
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.size(10.dp))
+        DescItem("RTL\npadding(left=2.dp, top=4.dp, right=6.dp, bottom=8.dp)") {
+            CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                Box(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .background(colorScheme.primaryContainer)
+                        .absolutePadding(left = 2.dp, top = 4.dp, right = 6.dp, bottom = 8.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(colorScheme.primary)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+private fun ModifierAbsolutePaddingSamplePreview() {
+    ModifierAbsolutePaddingSample(remember { MutableStateFlow(true) })
 }
 
 
@@ -147,7 +234,7 @@ private fun ModifierPaddingFromSample(allExpandFlow: Flow<Boolean>) {
                         .background(colorScheme.primaryContainer)
                 )
             }
-            DescItem("padding(top = 20.dp, bottom = 20.dp)") {
+            DescItem("padding(top=20.dp, bottom=20.dp)") {
                 Text(
                     text = "Compose",
                     modifier = Modifier
@@ -155,7 +242,7 @@ private fun ModifierPaddingFromSample(allExpandFlow: Flow<Boolean>) {
                         .padding(top = 20.dp, bottom = 20.dp)
                 )
             }
-            DescItem("paddingFromBaseline(top = 20.dp, bottom = 20.dp)") {
+            DescItem("paddingFromBaseline(top=20.dp, bottom=20.dp)") {
                 Text(
                     text = "Compose",
                     modifier = Modifier
