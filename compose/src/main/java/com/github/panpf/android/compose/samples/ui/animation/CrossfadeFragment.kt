57f519adf8b405cb3af2959003794b6531506b23
panpf <panpfpanpf@outlook.com>
2022-12-27 17:37:55 +0800

    new: Added rememberInfiniteTransition samples

--- compose/src/main/java/com/github/panpf/android/compose/samples/ui/animation/CrossfadeFragment.kt
+++ compose/src/main/java/com/github/panpf/android/compose/samples/ui/animation/CrossfadeFragment.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.panpf.android.compose.samples.R
@@ -45,16 +46,21 @@ private fun CrossfadeSample(allExpandFlow: Flow<Boolean>) {
     var enabled by remember { mutableStateOf(true) }
     ExpandableItem3(title = "Crossfade", allExpandFlow, padding = 20.dp) {
         Crossfade(targetState = enabled) {
+            val size = 160.dp
             if (it) {
                 val text =
-                    "躲过了暴风雪之后，我们再次起程赶路，在一处斜坡下发现了阿宁他们的马队，同时也发现了海底墓穴影画之中的那一座神秘雪山，赫然出现在了我们的视野尽头。就在我们询问向导如何才能到达那里的时候，顺子却摇头，说我们绝对无法过去。\n          ----摘自《盗墓笔记》 - 云顶天宫（下）第一章 五圣雪山，网址：http://www.daomubiji.com/yun-ding-tian-gong-15.html"
-                Text(text = text, modifier = Modifier.height(200.dp))
+                    "躲过了暴风雪之后，我们再次起程赶路，在一处斜坡下发现了阿宁他们的马队，同时也发现了海底墓穴影画之中的那一座神秘雪山，赫然出现在了我们的视野尽头。就在我们询问向导如何才能到达那里的时候，顺子却摇头，说我们绝对无法过去。"
+                Text(
+                    text = text,
+                    modifier = Modifier.height(size),
+                    overflow = TextOverflow.Ellipsis
+                )
             } else {
                 Image(
                     painter = painterResource(id = R.drawable.dog_hor),
                     contentDescription = "dog",
                     modifier = Modifier
-                        .size(200.dp)
+                        .size(size)
                         .clip(RoundedCornerShape(50)),
                     contentScale = ContentScale.Crop,
                 )
---
+++ compose/src/main/java/com/github/panpf/android/compose/samples/ui/animation/RememberInfiniteTransitionFragment.kt
@@ -0,0 +1,110 @@
+package com.github.panpf.android.compose.samples.ui.animation
+
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateValue
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.panpf.android.compose.samples.ui.base.ExpandableItem3
+import com.github.panpf.android.compose.samples.ui.base.ExpandableLayout
+import com.github.panpf.android.compose.samples.ui.base.Material3ComposeAppBarFragment
+import com.github.panpf.android.compose.samples.ui.base.MyColor
+import com.github.panpf.tools4j.math.ktx.format
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class RememberInfiniteTransitionFragment : Material3ComposeAppBarFragment() {
+
+    override fun getTitle(): String {
+        return "rememberInfiniteTransition"
+    }
+
+    @Composable
+    override fun DrawContent() {
+        ExpandableLayout { allExpandFlow ->
+            RememberInfiniteTransitionSample(allExpandFlow)
+        }
+    }
+}
+
+@Composable
+private fun RememberInfiniteTransitionSample(allExpandFlow: Flow<Boolean>) {
+    val transition = rememberInfiniteTransition()
+    val background by transition.animateColor(
+        initialValue = MyColor.HalfBlue,
+        targetValue = MyColor.HalfMagenta,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val size by transition.animateFloat(
+        initialValue = 50f,
+        targetValue = 80f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val roundedCornerPercent by transition.animateValue(
+        initialValue = 50,
+        targetValue = 20,
+        typeConverter = Int.VectorConverter,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    ExpandableItem3(title = "updateTransition", allExpandFlow, padding = 20.dp) {
+        Box(
+            modifier = Modifier
+                .border(1.dp, MaterialTheme.colorScheme.primary.copy(alpha = 0.5f))
+                .size(100.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(roundedCornerPercent))
+                    .background(background)
+                    .size(size.dp, size.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "background: (" +
+                    "red=${background.red.format("", 1)}, " +
+                    "green=${background.green.format("", 1)}, " +
+                    "blue=${background.blue.format("", 1)}" +
+                    ")"
+        )
+        Text(text = "size: (width=${size.toInt()}.dp, height=${size.toInt()}.dp)")
+        Text(text = "RoundedCornerShape percent: $roundedCornerPercent")
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+private fun RememberInfiniteTransitionSamplePreview() {
+    RememberInfiniteTransitionSample(remember { MutableStateFlow(true) })
+}
