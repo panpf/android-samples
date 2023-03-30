bd8656a927410330bc9e6ac0a346eb31be2e2b29
panpf <panpfpanpf@outlook.com>
2023-03-30 18:19:11 +0800

    improve: Improved Badge samples

--- compose/src/main/java/com/github/panpf/android/compose/samples/ui/material/BadgeFragment.kt
+++ compose/src/main/java/com/github/panpf/android/compose/samples/ui/material/BadgeFragment.kt
@@ -1,5 +1,10 @@
 package com.github.panpf.android.compose.samples.ui.material
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Badge
 import androidx.compose.material.BadgedBox
 import androidx.compose.material.Icon
@@ -8,15 +13,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.github.panpf.android.compose.samples.ui.base.ExpandableItem
-import com.github.panpf.android.compose.samples.ui.base.ExpandableLayout
 import com.github.panpf.android.compose.samples.ui.base.MaterialComposeAppBarFragment
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.google.accompanist.flowlayout.FlowRow
 
 class BadgeFragment : MaterialComposeAppBarFragment() {
 
@@ -26,22 +28,73 @@ class BadgeFragment : MaterialComposeAppBarFragment() {
 
     @Composable
     override fun DrawContent() {
-        ExpandableLayout { allExpandFlow ->
-            BadgeSample(allExpandFlow)
-            BadgeColorsSample(allExpandFlow)
-            BadgeIconSample(allExpandFlow)
-            BadgePointSample(allExpandFlow)
-            BadgeBoxSample(allExpandFlow)
-        }
+        BadgeSample()
     }
 }
 
 
 @Composable
-private fun BadgeSample(allExpandFlow: Flow<Boolean>) {
-    ExpandableItem(title = "Badge", allExpandFlow, padding = 20.dp) {
-        Badge {
-            Text(text = "99+")
+private fun BadgeSample() {
+    FlowRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp), mainAxisSpacing = 20.dp, crossAxisSpacing = 20.dp
+    ) {
+        Column {
+            Text(text = "Default")
+            Spacer(modifier = Modifier.size(10.dp))
+            Badge {
+                Text(text = "99+")
+            }
+        }
+
+        Column {
+            Text(text = "backgroundColor")
+            Spacer(modifier = Modifier.size(10.dp))
+            Badge(backgroundColor = Color.Blue) {
+                Text(text = "99+")
+            }
+        }
+
+        Column {
+            Text(text = "contentColor")
+            Spacer(modifier = Modifier.size(10.dp))
+            Badge(contentColor = Color.Green) {
+                Text(text = "99+")
+            }
+        }
+
+        Column {
+            Text(text = "Icon")
+            Spacer(modifier = Modifier.size(10.dp))
+            Badge {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+        }
+
+        Column {
+            Text(text = "Point")
+            Spacer(modifier = Modifier.size(10.dp))
+            Badge()
+        }
+
+        Column {
+            Text(text = "Box")
+            Spacer(modifier = Modifier.size(10.dp))
+            BadgedBox(badge = {
+                Badge {
+                    Text(text = "99+")
+                }
+            }) {
+                Icon(
+                    Icons.Filled.Settings,
+                    contentDescription = "Favorite"
+                )
+            }
         }
     }
 }
@@ -49,78 +102,5 @@ private fun BadgeSample(allExpandFlow: Flow<Boolean>) {
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF)
 @Composable
 private fun BadgeSamplePreview() {
-    BadgeSample(remember { MutableStateFlow(true) })
-}
-
-
-@Composable
-private fun BadgeColorsSample(allExpandFlow: Flow<Boolean>) {
-    ExpandableItem(title = "Badge（colors）", allExpandFlow, padding = 20.dp) {
-        Badge(backgroundColor = Color.Blue, contentColor = Color.Green) {
-            Text(text = "99+")
-        }
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
-@Composable
-private fun BadgeColorsSamplePreview() {
-    BadgeColorsSample(remember { MutableStateFlow(true) })
-}
-
-
-@Composable
-private fun BadgeIconSample(allExpandFlow: Flow<Boolean>) {
-    ExpandableItem(title = "Badge（Icon）", allExpandFlow, padding = 20.dp) {
-        Badge {
-            Icon(
-                imageVector = Icons.Filled.Close,
-                contentDescription = null,
-                tint = Color.White
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
-@Composable
-private fun BadgeIconSamplePreview() {
-    BadgeIconSample(remember { MutableStateFlow(true) })
-}
-
-
-@Composable
-private fun BadgePointSample(allExpandFlow: Flow<Boolean>) {
-    ExpandableItem(title = "Badge（Point）", allExpandFlow, padding = 20.dp) {
-        Badge()
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
-@Composable
-private fun BadgePointSamplePreview() {
-    BadgePointSample(remember { MutableStateFlow(true) })
-}
-
-
-@Composable
-private fun BadgeBoxSample(allExpandFlow: Flow<Boolean>) {
-    ExpandableItem(title = "Badge（Box）", allExpandFlow, padding = 20.dp) {
-        BadgedBox(badge = {
-            Badge {
-                Text(text = "99+")
-            }
-        }) {
-            Icon(
-                Icons.Filled.Settings,
-                contentDescription = "Favorite"
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
-@Composable
-private fun BadgeBoxSamplePreview() {
-    BadgeBoxSample(remember { MutableStateFlow(true) })
+    BadgeSample()
 }
--- compose/src/main/java/com/github/panpf/android/compose/samples/ui/material3/BadgeFragment.kt
+++ compose/src/main/java/com/github/panpf/android/compose/samples/ui/material3/BadgeFragment.kt
@@ -1,5 +1,10 @@
 package com.github.panpf.android.compose.samples.ui.material3
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Settings
@@ -9,15 +14,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.github.panpf.android.compose.samples.ui.base.ExpandableItem3
-import com.github.panpf.android.compose.samples.ui.base.ExpandableLayout
 import com.github.panpf.android.compose.samples.ui.base.Material3ComposeAppBarFragment
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.google.accompanist.flowlayout.FlowRow
 
 class BadgeFragment : Material3ComposeAppBarFragment() {
 
@@ -27,23 +29,74 @@ class BadgeFragment : Material3ComposeAppBarFragment() {
 
     @Composable
     override fun DrawContent() {
-        ExpandableLayout { allExpandFlow ->
-            BadgeSample(allExpandFlow)
-            BadgeColorsSample(allExpandFlow)
-            BadgeIconSample(allExpandFlow)
-            BadgePointSample(allExpandFlow)
-            BadgeBoxSample(allExpandFlow)
-        }
+        BadgeSample()
     }
 }
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun BadgeSample(allExpandFlow: Flow<Boolean>) {
-    ExpandableItem3(title = "Badge", allExpandFlow, padding = 20.dp) {
-        Badge {
-            Text(text = "99+")
+private fun BadgeSample() {
+    FlowRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp), mainAxisSpacing = 20.dp, crossAxisSpacing = 20.dp
+    ) {
+        Column {
+            Text(text = "Default")
+            Spacer(modifier = Modifier.size(10.dp))
+            Badge {
+                Text(text = "99+")
+            }
+        }
+
+        Column {
+            Text(text = "containerColor")
+            Spacer(modifier = Modifier.size(10.dp))
+            Badge(containerColor = Color.Blue) {
+                Text(text = "99+")
+            }
+        }
+
+        Column {
+            Text(text = "contentColor")
+            Spacer(modifier = Modifier.size(10.dp))
+            Badge(contentColor = Color.Green) {
+                Text(text = "99+")
+            }
+        }
+
+        Column {
+            Text(text = "Icon")
+            Spacer(modifier = Modifier.size(10.dp))
+            Badge {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+        }
+
+        Column {
+            Text(text = "Point")
+            Spacer(modifier = Modifier.size(10.dp))
+            Badge()
+        }
+
+        Column {
+            Text(text = "Box")
+            Spacer(modifier = Modifier.size(10.dp))
+            BadgedBox(badge = {
+                Badge {
+                    Text(text = "99+")
+                }
+            }) {
+                Icon(
+                    Icons.Filled.Settings,
+                    contentDescription = "Favorite"
+                )
+            }
         }
     }
 }
@@ -51,82 +104,5 @@ private fun BadgeSample(allExpandFlow: Flow<Boolean>) {
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF)
 @Composable
 private fun BadgeSamplePreview() {
-    BadgeSample(remember { MutableStateFlow(true) })
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun BadgeColorsSample(allExpandFlow: Flow<Boolean>) {
-    ExpandableItem3(title = "Badge（colors）", allExpandFlow, padding = 20.dp) {
-        Badge(containerColor = Color.Blue, contentColor = Color.Green) {
-            Text(text = "99+")
-        }
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
-@Composable
-private fun BadgeColorsSamplePreview() {
-    BadgeColorsSample(remember { MutableStateFlow(true) })
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun BadgeIconSample(allExpandFlow: Flow<Boolean>) {
-    ExpandableItem3(title = "Badge（Icon）", allExpandFlow, padding = 20.dp) {
-        Badge {
-            Icon(
-                imageVector = Icons.Filled.Close,
-                contentDescription = null,
-                tint = Color.White
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
-@Composable
-private fun BadgeIconSamplePreview() {
-    BadgeIconSample(remember { MutableStateFlow(true) })
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun BadgePointSample(allExpandFlow: Flow<Boolean>) {
-    ExpandableItem3(title = "Badge（Point）", allExpandFlow, padding = 20.dp) {
-        Badge()
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
-@Composable
-private fun BadgePointSamplePreview() {
-    BadgePointSample(remember { MutableStateFlow(true) })
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun BadgeBoxSample(allExpandFlow: Flow<Boolean>) {
-    ExpandableItem3(title = "Badge（Box）", allExpandFlow, padding = 20.dp) {
-        BadgedBox(badge = {
-            Badge {
-                Text(text = "99+")
-            }
-        }) {
-            Icon(
-                Icons.Filled.Settings,
-                contentDescription = "Favorite"
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
-@Composable
-private fun BadgeBoxSamplePreview() {
-    BadgeBoxSample(remember { MutableStateFlow(true) })
+    BadgeSample()
 }
