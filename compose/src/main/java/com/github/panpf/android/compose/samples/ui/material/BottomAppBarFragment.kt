60a5270f19cc846eaad8b2189596044cf86f8362
panpf <panpfpanpf@outlook.com>
2023-04-03 17:31:55 +0800

    improve: Improved BottomAppBar samples

--- compose/src/main/java/com/github/panpf/android/compose/samples/ui/material/BottomAppBarFragment.kt
+++ compose/src/main/java/com/github/panpf/android/compose/samples/ui/material/BottomAppBarFragment.kt
@@ -1,12 +1,18 @@
 package com.github.panpf.android.compose.samples.ui.material
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Phone
@@ -19,13 +25,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.github.panpf.android.compose.samples.ui.base.ExpandableItem
-import com.github.panpf.android.compose.samples.ui.base.ExpandableLayout
 import com.github.panpf.android.compose.samples.ui.base.MaterialComposeAppBarFragment
 import com.github.panpf.android.compose.samples.ui.base.MyColor
 import com.github.panpf.tools4a.toast.ktx.showShortToast
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 
 class BottomAppBarFragment : MaterialComposeAppBarFragment() {
 
@@ -35,16 +37,13 @@ class BottomAppBarFragment : MaterialComposeAppBarFragment() {
 
     @Composable
     override fun DrawContent() {
-        ExpandableLayout { allExpandFlow ->
-            BottomAppBarSample(allExpandFlow)
-            BottomAppBarColorsSample(allExpandFlow)
-        }
+        BottomAppBarSample()
     }
 }
 
 
 @Composable
-private fun BottomAppBarSample(allExpandFlow: Flow<Boolean>) {
+private fun BottomAppBarSample() {
     val items = remember {
         listOf(
             "首页" to Icons.Filled.Home,
@@ -54,22 +53,89 @@ private fun BottomAppBarSample(allExpandFlow: Flow<Boolean>) {
         )
     }
     val context = LocalContext.current
-    ExpandableItem(title = "BottomAppBar", allExpandFlow, padding = 20.dp) {
-        Column {
-            BottomAppBar {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+            .padding(20.dp)
+    ) {
+        Text(text = "Default")
+        Spacer(modifier = Modifier.size(10.dp))
+        BottomAppBar {
+            items.forEach {
+                IconButton(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f),
+                    onClick = { context.showShortToast(it.first) }
                 ) {
-                    items.forEach {
-                        IconButton(
-                            modifier = Modifier
-                                .fillMaxHeight()
-                                .weight(1f),
-                            onClick = { context.showShortToast(it.first) }
-                        ) {
-                            Icon(imageVector = it.second, contentDescription = it.first)
-                        }
-                    }
+                    Icon(imageVector = it.second, contentDescription = it.first)
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.size(20.dp))
+        Text(text = "backgroundColor")
+        Spacer(modifier = Modifier.size(10.dp))
+        BottomAppBar(backgroundColor = MyColor.TranslucenceRed) {
+            items.forEach {
+                IconButton(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f),
+                    onClick = { context.showShortToast(it.first) }
+                ) {
+                    Icon(imageVector = it.second, contentDescription = it.first)
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.size(20.dp))
+        Text(text = "contentColor")
+        Spacer(modifier = Modifier.size(10.dp))
+        BottomAppBar(contentColor = Color.Red) {
+            items.forEach {
+                IconButton(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f),
+                    onClick = { context.showShortToast(it.first) }
+                ) {
+                    Icon(imageVector = it.second, contentDescription = it.first)
+                }
+            }
+        }
+
+        // cutoutShape Valid only in Scaffold
+
+        Spacer(modifier = Modifier.size(20.dp))
+        Text(text = "elevation=15.dp")
+        Spacer(modifier = Modifier.size(10.dp))
+        BottomAppBar(elevation = 15.dp) {
+            items.forEach {
+                IconButton(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f),
+                    onClick = { context.showShortToast(it.first) }
+                ) {
+                    Icon(imageVector = it.second, contentDescription = it.first)
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.size(20.dp))
+        Text(text = "contentPadding=PaddingValues(horizontal=20.dp, vertical=12.dp)")
+        Spacer(modifier = Modifier.size(10.dp))
+        BottomAppBar(contentPadding = PaddingValues(horizontal = 20.dp, vertical = 10.dp)) {
+            items.forEach {
+                IconButton(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f),
+                    onClick = { context.showShortToast(it.first) }
+                ) {
+                    Icon(imageVector = it.second, contentDescription = it.first)
                 }
             }
         }
@@ -79,48 +145,5 @@ private fun BottomAppBarSample(allExpandFlow: Flow<Boolean>) {
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF)
 @Composable
 private fun BottomAppBarSamplePreview() {
-    BottomAppBarSample(remember { MutableStateFlow(true) })
-}
-
-
-@Composable
-private fun BottomAppBarColorsSample(allExpandFlow: Flow<Boolean>) {
-    val items = remember {
-        listOf(
-            "首页" to Icons.Filled.Home,
-            "通讯录" to Icons.Filled.Phone,
-            "游戏" to Icons.Filled.PlayArrow,
-            "设置" to Icons.Filled.Settings,
-        )
-    }
-    val context = LocalContext.current
-    ExpandableItem(title = "BottomAppBar（colors）", allExpandFlow, padding = 20.dp) {
-        Column {
-            BottomAppBar(
-                backgroundColor = MyColor.TranslucenceRed,
-                contentColor = Color.Red,
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                ) {
-                    items.forEach {
-                        IconButton(
-                            modifier = Modifier
-                                .fillMaxHeight()
-                                .weight(1f),
-                            onClick = { context.showShortToast(it.first) }
-                        ) {
-                            Icon(imageVector = it.second, contentDescription = it.first)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
-@Composable
-private fun BottomAppBarColorsSamplePreview() {
-    BottomAppBarColorsSample(remember { MutableStateFlow(true) })
+    BottomAppBarSample()
 }
--- compose/src/main/java/com/github/panpf/android/compose/samples/ui/material3/BottomAppBarFragment.kt
+++ compose/src/main/java/com/github/panpf/android/compose/samples/ui/material3/BottomAppBarFragment.kt
@@ -1,11 +1,15 @@
 package com.github.panpf.android.compose.samples.ui.material3
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
@@ -19,16 +23,12 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.github.panpf.android.compose.samples.ui.base.ExpandableItem3
-import com.github.panpf.android.compose.samples.ui.base.ExpandableLayout
 import com.github.panpf.android.compose.samples.ui.base.Material3ComposeAppBarFragment
+import com.github.panpf.android.compose.samples.ui.base.theme.MyThemeColors3
 import com.github.panpf.tools4a.toast.ktx.showShortToast
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 
 class BottomAppBarFragment : Material3ComposeAppBarFragment() {
 
@@ -38,16 +38,13 @@ class BottomAppBarFragment : Material3ComposeAppBarFragment() {
 
     @Composable
     override fun DrawContent() {
-        ExpandableLayout { allExpandFlow ->
-            BottomAppBarSample(allExpandFlow)
-            BottomAppBarColorsSample(allExpandFlow)
-        }
+        BottomAppBarSample()
     }
 }
 
 
 @Composable
-private fun BottomAppBarSample(allExpandFlow: Flow<Boolean>) {
+private fun BottomAppBarSample() {
     val items = remember {
         listOf(
             "首页" to Icons.Filled.Home,
@@ -57,117 +54,133 @@ private fun BottomAppBarSample(allExpandFlow: Flow<Boolean>) {
         )
     }
     val context = LocalContext.current
-    ExpandableItem3(title = "BottomAppBar", allExpandFlow, padding = 20.dp) {
-        Column {
-            BottomAppBar(
-                actions = {
-                    items.forEach {
-                        IconButton(
-                            modifier = Modifier.fillMaxHeight(),
-                            onClick = { context.showShortToast(it.first) }
-                        ) {
-                            Icon(imageVector = it.second, contentDescription = it.first)
-                        }
-                    }
-                },
-                floatingActionButton = {
-                    FloatingActionButton(onClick = { context.showShortToast("add") }) {
-                        Icon(
-                            imageVector = Icons.Filled.Add,
-                            contentDescription = "add"
-                        )
-                    }
-                }
-            )
-            Spacer(modifier = Modifier.size(10.dp))
-            BottomAppBar {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
+    val colors = MyThemeColors3.current
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+            .padding(20.dp)
+    ) {
+        Text(text = "Default")
+        Spacer(modifier = Modifier.size(10.dp))
+        BottomAppBar {
+            items.forEach {
+                IconButton(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f),
+                    onClick = { context.showShortToast(it.first) }
                 ) {
-                    items.forEach {
-                        IconButton(
-                            modifier = Modifier
-                                .fillMaxHeight()
-                                .weight(1f),
-                            onClick = { context.showShortToast(it.first) }
-                        ) {
-                            Icon(imageVector = it.second, contentDescription = it.first)
-                        }
-                    }
+                    Icon(imageVector = it.second, contentDescription = it.first)
                 }
             }
         }
+
+        Spacer(modifier = Modifier.size(20.dp))
+        Text(text = "floatingActionButton")
+        Spacer(modifier = Modifier.size(10.dp))
+        BottomAppBar(
+            actions = {
+                items.forEach {
+                    IconButton(
+                        modifier = Modifier.fillMaxHeight(),
+                        onClick = { context.showShortToast(it.first) }
+                    ) {
+                        Icon(imageVector = it.second, contentDescription = it.first)
+                    }
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { context.showShortToast("add") }) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "add"
+                    )
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.size(20.dp))
+        Text(text = "containerColor")
+        Spacer(modifier = Modifier.size(10.dp))
+        BottomAppBar(
+            actions = {
+                items.forEach {
+                    IconButton(
+                        modifier = Modifier.fillMaxHeight(),
+                        onClick = { context.showShortToast(it.first) }
+                    ) {
+                        Icon(imageVector = it.second, contentDescription = it.first)
+                    }
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { context.showShortToast("add") }) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "add"
+                    )
+                }
+            },
+            containerColor = colors.tertiaryContainer,
+        )
+
+        Spacer(modifier = Modifier.size(20.dp))
+        Text(text = "contentColor")
+        Spacer(modifier = Modifier.size(10.dp))
+        BottomAppBar(
+            actions = {
+                items.forEach {
+                    IconButton(
+                        modifier = Modifier.fillMaxHeight(),
+                        onClick = { context.showShortToast(it.first) }
+                    ) {
+                        Icon(imageVector = it.second, contentDescription = it.first)
+                    }
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { context.showShortToast("add") }) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "add"
+                    )
+                }
+            },
+            contentColor = colors.tertiary,
+        )
+        
+        // todo tonalElevation invalid
+
+        Spacer(modifier = Modifier.size(20.dp))
+        Text(text = "contentPadding=PaddingValues(horizontal=20.dp, vertical=12.dp)")
+        Spacer(modifier = Modifier.size(10.dp))
+        BottomAppBar(
+            actions = {
+                items.forEach {
+                    IconButton(
+                        modifier = Modifier.fillMaxHeight(),
+                        onClick = { context.showShortToast(it.first) }
+                    ) {
+                        Icon(imageVector = it.second, contentDescription = it.first)
+                    }
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { context.showShortToast("add") }) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "add"
+                    )
+                }
+            },
+            contentPadding = PaddingValues(horizontal = 20.dp, vertical = 10.dp),
+        )
     }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF)
 @Composable
 private fun BottomAppBarSamplePreview() {
-    BottomAppBarSample(remember { MutableStateFlow(true) })
-}
-
-
-@Composable
-private fun BottomAppBarColorsSample(allExpandFlow: Flow<Boolean>) {
-    val items = remember {
-        listOf(
-            "首页" to Icons.Filled.Home,
-            "通讯录" to Icons.Filled.Phone,
-            "游戏" to Icons.Filled.PlayArrow,
-            "设置" to Icons.Filled.Settings,
-        )
-    }
-    val context = LocalContext.current
-    ExpandableItem3(title = "BottomAppBar（colors）", allExpandFlow, padding = 20.dp) {
-        Column {
-            BottomAppBar(
-                actions = {
-                    items.forEach {
-                        IconButton(
-                            modifier = Modifier.fillMaxHeight(),
-                            onClick = { context.showShortToast(it.first) }
-                        ) {
-                            Icon(imageVector = it.second, contentDescription = it.first)
-                        }
-                    }
-                },
-                floatingActionButton = {
-                    FloatingActionButton(onClick = { context.showShortToast("add") }) {
-                        Icon(
-                            imageVector = Icons.Filled.Add,
-                            contentDescription = "add"
-                        )
-                    }
-                },
-                containerColor = Color.Blue.copy(alpha = 0.6f),
-                contentColor = Color.White,
-            )
-            Spacer(modifier = Modifier.size(10.dp))
-            BottomAppBar(
-                containerColor = Color.Blue.copy(alpha = 0.6f),
-                contentColor = Color.White,
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                ) {
-                    items.forEach {
-                        IconButton(
-                            modifier = Modifier
-                                .fillMaxHeight()
-                                .weight(1f),
-                            onClick = { context.showShortToast(it.first) }
-                        ) {
-                            Icon(imageVector = it.second, contentDescription = it.first)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
-@Composable
-private fun BottomAppBarColorsSamplePreview() {
-    BottomAppBarColorsSample(remember { MutableStateFlow(true) })
+    BottomAppBarSample()
 }
